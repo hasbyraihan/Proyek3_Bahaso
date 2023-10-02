5b5427cd97e77ded3e260354918c9e10694e1c8c
haalzi <afis.kian2015@gmail.com>
2023-10-02 21:40:42 +0700

    Test commit

---
+++ contoh/database/migrations/2023_09_27_052223_create_unemployeds_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('unemployeds', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->enum('jenkel', ['Laki-laki', 'Perempuan']);
+            $table->integer('notelp');
+            $table->string('alamat');
+            $table->integer('kodepos');
+            $table->string('kota');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('unemployeds');
+    }
+};
