82cb26ad7ec909e6a2a2af4baa8ede03aeac7178
hasbyraihan <hasby608@gmail.com>
2023-10-18 00:01:27 +0700

    alhamdulillah sudah di edit tampilannyaaah

--- contoh/resources/views/tambahpengangguran.blade.php
+++ contoh/resources/views/tambahpengangguran.blade.php
@@ -7,10 +7,23 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
     <title>Data Pengangguran</title>
   </head>
   <body>
+    <div class="nav">
+      <div class="logo">
+          <a href="#">
+              <p>Ci<span>Vi</span></p>
+          </a>
+      </div>
+      <ul>
+          <li> <a class="nav1" href="http://127.0.0.1:8000/tambahpengangguran">Create CV</a> </li>
+          <li> <a class="nav1" href="http://127.0.0.1:8000/tambahpengangguran">Templates</a> </li>
+          <li> <a class="nav1" href="http://127.0.0.1:8000/tambahpengangguran"> Login </a> </li>
+      </ul>
+  </div>
+
     <h1 class="text-center mb-5">Tambah data Pengangguran</h1>
     <div class="container">
       <div class="row justify-content-center " > 
@@ -63,12 +76,63 @@
           </div>
         </div>
 
+      </div>
+      <div class="footer">
+        <div class="row"> 
+            <div class="footer-col">
+                <h3> Create your resume in a minute,<br> get your dream job in a blink.</h3>
+                <div class="social-media">
+                    <a href="https://www.facebook.com">
+                        <img src="img/Facebook.png" alt="">
+                    </a>
+                    <a href="https://www.instagram.com">
+                        <img src="img/Instagram.png" alt="">
+                    </a>
+                    <a href="https://www.linkedin.com">
+                        <img src="img/LinkedIn.png" alt="">
+                    </a>
+                    <a href="https://www.twitter.com">
+                        <img src="img/Twitter.png" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="footer-col">
+                    <h4>Terms & Policies </h4>
+                    <ul>
+                        <li><a href="#">Terms of Service</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul>
+            </div>
+            <div class="footer-col">
+                    <h4>Company</h4>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+            </div>
+            <div class="footer-col">
+                    <h4>Contact </h4>
+                    <ul>
+                        <li><a href="#">090932</a></li>
+                        <li><a href="#">hasby608@gmail.com</a></li>
+                    </ul>
+            </div>
+            <div class="garis">
+                <hr style=" width:1170px; margin-top: 80px; margin-left: 130px; color:#FFFFFF;">
+            </div>
+
+            <div class="copyright">
+                <h6>Copyright, CiVi 2023. All rights reserved.</h6>
+            </div>
+
+        </div>
+    </div>
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-      </div>
-    </div>
   </body>
 </html>
--- contoh/resources/views/welcome.blade.php
+++ contoh/resources/views/welcome.blade.php
@@ -7,9 +7,10 @@
         <title>Proyek 3</title>
         <link rel="stylesheet" href="{{ asset('css/app.css')}}">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        
+        <script src="fontawesome/all.js"></script>
     </head>
     <body>
+
         <div class="wrapper">
             <div class="nav">
                 <div class="logo">
@@ -21,8 +22,6 @@
                     <li> <a class="nav1" href="http://127.0.0.1:8000/tambahpengangguran">Create CV</a> </li>
                     <li> <a class="nav1" href="http://127.0.0.1:8000/tambahpengangguran">Templates</a> </li>
                     <li> <a class="nav1" href="http://127.0.0.1:8000/tambahpengangguran"> Login </a> </li>
-                    
-                    
                 </ul>
             </div>
             <div class="header">
@@ -33,12 +32,64 @@
             <div class="content-header">
                 <p> Create Impressive CVs in Minutes!.</p>
             </div>
-            
+             
             <div class="abutton">
-                <a href="/pengangguran" class="btn p-3 rounded-7" tabindex="-1" role="button" aria-disabled="true">Create your CV</a>
+                <a href="/tambahpengangguran" class="btn p-3 rounded-7" tabindex="-1" role="button" aria-disabled="true">Create your CV</a>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="row"> 
+            <div class="footer-col">
+                <h3> Create your resume in a minute,<br> get your dream job in a blink.</h3>
+                <div class="social-media">
+                    <a href="https://www.facebook.com">
+                        <img src="img/Facebook.png" alt="">
+                    </a>
+                    <a href="https://www.instagram.com">
+                        <img src="img/Instagram.png" alt="">
+                    </a>
+                    <a href="https://www.linkedin.com">
+                        <img src="img/LinkedIn.png" alt="">
+                    </a>
+                    <a href="https://www.twitter.com">
+                        <img src="img/Twitter.png" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="footer-col">
+                    <h4>Terms & Policies </h4>
+                    <ul>
+                        <li><a href="#">Terms of Service</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul>
+            </div>
+            <div class="footer-col">
+                    <h4>Company</h4>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+            </div>
+            <div class="footer-col">
+                    <h4>Contact </h4>
+                    <ul>
+                        <li><a href="#">090932</a></li>
+                        <li><a href="#">hasby608@gmail.com</a></li>
+                    </ul>
+            </div>
+            <div class="garis">
+                <hr style=" width:1170px; margin-top: 80px; margin-left: 130px; color:#FFFFFF;">
+            </div>
+
+            <div class="copyright">
+                <h6>Copyright, CiVi 2023. All rights reserved.</h6>
             </div>
 
         </div>
+    </div>
+    
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </body>
 </html>
