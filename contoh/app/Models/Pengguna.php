2cf404686b2c3dabff8a93af7279f8f8dd65c9c1
hasbyraihan <hasby608@gmail.com>
2023-11-15 10:43:55 +0700

    login dan register sudah diperbaharui

--- contoh/app/Models/Pengguna.php
+++ contoh/app/Models/Pengguna.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pengguna extends Model
-{
+{   
     use HasFactory;
 }
--- contoh/resources/views/login.blade.php
+++ contoh/resources/views/login.blade.php
@@ -26,11 +26,23 @@
     </style>
 </head>
 <body>
+  <div class="nav">
+    <div class="logo">
+        <a href="http://127.0.0.1:8000/">
+            <p>Ci<span>Vi</span></p>
+        </a>
+    </div>
+    <ul>
+        <li> <a class="nav1" href="http://127.0.0.1:8000/tambahpengangguran">Create CV</a> </li>
+        <li> <a class="nav1" href="http://127.0.0.1:8000/tambahpengangguran">Templates</a> </li>
+        <li> <a class="nav1" href="http://127.0.0.1:8000/login_aja"> Login </a> </li>
+    </ul> 
+  </div>
 <section class="vh-100">
   <div class="container-fluid h-custom">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-md-9 col-lg-6 col-xl-5">
-        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+        <img src="img/Logins.png"
           class="img-fluid" alt="Sample image">
       </div>
       <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
@@ -51,41 +63,69 @@
           </div>
 
           <div class="text-center text-lg-start mt-4 pt-2">
-            <button type="submit" class="btn btn-primary btn-lg"
+            <button type="submit" class="btn btn-success btn-lg"
               style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
             <p class="small fw-bold mt-2 pt-1 mb-0">Belum mempunyai akun? <a href="/register"
-                class="link-danger">Daftar</a></p>
+                class="link-primary">Daftar</a></p>
           </div>
 
         </form>
       </div>
     </div>
   </div>
-  <div
-    class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
-    <!-- Copyright -->
-    <div class="text-white mb-3 mb-md-0">
-      Copyright © 2020. All rights reserved.
-    </div>
-    <!-- Copyright -->
+  <div class="footer">
+    <div class="row"> 
+        <div class="footer-col">
+            <h3> Create your resume in a minute,<br> get your dream job in a blink.</h3>
+            <div class="social-media">
+                <a href="https://www.facebook.com">
+                    <img src="img/Facebook.png" alt="">
+                </a>
+                <a href="https://www.instagram.com">
+                    <img src="img/Instagram.png" alt="">
+                </a>
+                <a href="https://www.linkedin.com">
+                    <img src="img/LinkedIn.png" alt="">
+                </a>
+                <a href="https://www.twitter.com">
+                    <img src="img/Twitter.png" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="footer-col">
+                <h4>Terms & Policies </h4>
+                <ul>
+                    <li><a href="#">Terms of Service</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                </ul>
+        </div>
+        <div class="footer-col">
+                <h4>Company</h4>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+        </div>
+        <div class="footer-col">
+                <h4>Contact </h4>
+                <ul>
+                    <li><a href="#">090932</a></li>
+                    <li><a href="#">hasby608@gmail.com</a></li>
+                </ul>
+                       
+        
+        </div>
+        <div class="garis">
+            <hr style=" width:1170px; margin-top: 80px; margin-left: 130px; color:#FFFFFF;">
+        </div>
 
-    <!-- Right -->
-    <div>
-      <a href="#!" class="text-white me-4">
-        <i class="fab fa-facebook-f"></i>
-      </a>
-      <a href="#!" class="text-white me-4">
-        <i class="fab fa-twitter"></i>
-      </a>
-      <a href="#!" class="text-white me-4">
-        <i class="fab fa-google"></i>
-      </a>
-      <a href="#!" class="text-white">
-        <i class="fab fa-linkedin-in"></i>
-      </a>
+        <div class="copyright">
+            <h6>Copyright, CiVi 2023. All rights reserved.</h6>
+        </div>
+
     </div>
-    <!-- Right -->
-  </div>
+</div>
 </section>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 @include('sweetalert::alert')
